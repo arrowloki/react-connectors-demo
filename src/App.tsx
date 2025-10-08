a6811ea4206f4190f6f48c6202cde618b4045b1e
arrowloki <loki.naveen549@gmail.com>
2025-10-08 22:36:33 +0530

    basic axios fetch and styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Apps from "./Pages/Apps.tsx";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
+    return (
      <>
-        <h1>
-            Hello World!
-        </h1>
+         <Apps/>
      </>
   )
 }
---
+++ src/Pages/Apps.tsx
@@ -0,0 +1,31 @@
+import axios from 'axios';
+import {useEffect, useState} from "react";
+import AppTile from "../components/AppTile.tsx";
+const Apps = () => {
+    const [data, setData] = useState([]);
+    const getAppsData = async () => {
+       const response=  await axios.get('https://68e66b1f21dd31f22cc58fcc.mockapi.io/connectors/app');
+       console.log(response.data);
+       setData(response.data);
+    }
+    useEffect(() => {
+        getAppsData().catch(console.error);
+    },[])
+
+    return (
+        <div className="m-2">
+            <h1 className="font-bold text-3xl mb-5 display-flex flex-1 items-center justify-center text-center">
+                Your Apps
+            </h1>
+            <div className="grid sm:grid-cols-5 sm:gap-4 grid-cols-2 gap-6">
+                {data.map((item) => (
+                    <div key={item.id}>
+                        <AppTile  item = {item} />
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default Apps;
