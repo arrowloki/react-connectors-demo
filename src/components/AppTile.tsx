f68e71e0baa854b81ca1ce658f254c84dd377ecb
arrowloki <loki.naveen549@gmail.com>
2025-10-09 12:05:18 +0530

    error msg display, added AppDara type

--- src/components/AppTile.tsx
+++ src/components/AppTile.tsx
@@ -4,7 +4,7 @@ const AppTile = ({item} : {item : AppData}) => {
   return (
     <div className="flex flex-col items-center justify-center border border-gray-200 p-2">
         <img className="rounded-full" src={item?.icon} alt={item.name} style={{width: '80px'}} />
-        <span title={item.name}>{(item?.name.length > 8) ? item?.name?.slice(0,8) : item.name}</span>
+        <span title={item.name}>{(item?.name.length > 9) ? item?.name?.slice(0,8) : item.name}</span>
     </div>
   )
 }
